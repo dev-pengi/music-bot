a936d20257a0fa1ab1e4a443b5849ae6a723726d
dev-pengi <siffffdoul@gmail.com>
2023-05-20 18:58:56 +0100

    quick fix

--- handler.js
+++ handler.js
@@ -94,8 +94,7 @@ module.exports = (client) => {
   function setUpPlayed() {
     client.player = new Player(client, {
       ytdlOptions: {
-        quality: "highestaudio",
-        highWaterMark: 1 << 25
+        quality: "highest",
       },
       ffmpeg: 'C:\\ffmpeg\\bin',
       ffmpegArgs: [
