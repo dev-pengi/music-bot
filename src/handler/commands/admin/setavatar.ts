c6c789ccc6d1998fdc0e57a2580d111ddbe397bb
dev-pengi <siffffdoul@gmail.com>
2023-05-27 03:34:48 +0100

    Create setavatar command

---
+++ src/handler/commands/admin/setavatar.ts
@@ -0,0 +1,30 @@
+import { Client, Message } from "discord.js";
+import { embedReply } from "../../../events/tools";
+import isImageUrl from 'is-image-url';
+
+export default {
+    name: "setavatar",
+    description: "change the bot's avatar",
+    async run(message: Message, args: string[], client: Client): Promise<void> {
+        try {
+            const attachment = message.attachments.first()?.url ?? null;
+            if (!attachment)
+                throw new Error('يجب hvthr صورتك مع الامر');
+
+            if (!await isImageUrl(attachment))
+                throw new Error('هذه الصورة غير قابلة للاستعمال');
+
+            await client.user?.setAvatar(attachment);
+            embedReply({
+                message,
+                content: `تم تغيير افاتار البوت بنجاح`,
+            });
+        } catch (err: any) {
+            embedReply({
+                message,
+                content: err.message,
+                error: true,
+            });
+        }
+    },
+};
